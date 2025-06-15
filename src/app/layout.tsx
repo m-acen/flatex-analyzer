7fd773fe4a3982f74807a32f727a1c2b64ebc086
Seschoscho <j.higatzberger@gmail.com>
2025-06-15 17:07:09 +0200

    feat(seo): add metadata config for Open Graph and Twitter cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,39 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: "Flatex Analyzer",
+  description:
+    "Flatex Portfolio Statistik: Überblick über Dividenden, Verkäufe und Performance deines Flatex Depots",
+  keywords: [
+    "Flatex",
+    "Depot Auswertung",
+    "Portfolio Analyse",
+    "Flatex Statistik",
+    "Dividenden Tracking",
+  ],
+  openGraph: {
+    title: 'Flatex Analyzer',
+    description: "Flatex Portfolio Statistik: Überblick über Dividenden, Verkäufe und Performance deines Flatex Depots",
+    url: 'https://flatex-analyzer.jhiga.com',
+    siteName: 'Flatex Analyzer',
+    images: [
+      {
+        url: 'https://flatex-analyzer.jhiga.com/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Flatex Analyzer Vorschau',
+      },
+    ],
+    locale: 'de_DE',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Flatex Analyzer',
+    description: "Flatex Portfolio Statistik: Überblick über Dividenden, Verkäufe und Performance deines Flatex Depots",
+    images: ['https://flatex-analyzer.jhiga.com/og-image.png'],
+  },
+
+  metadataBase: new URL('https://flatex-analyzer.jhiga.com'),
 };
 
 export default function RootLayout({
@@ -23,11 +56,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={roboto.variable}>
-      <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <body>
         <div className="min-h-svh flex flex-col justify-between">
           <ClientWrapper>
-            <Header />{children}
+            <Header />
+            {children}
             <Footer />
           </ClientWrapper>
         </div>
